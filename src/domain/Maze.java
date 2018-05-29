8a0c417f91c9cf26825db13358c3acf1a45faa6b
melvinastorga <melvin.astorga@gmail.com>
2018-05-29 09:44:09 -0600

    matriz easy

--- src/domain/Maze.java
+++ src/domain/Maze.java
@@ -78,18 +78,76 @@ public class Maze{
     public int [][] fillMaze (){
         
         int [][] mazeFilled= new int [mazeItems.length][mazeItems.length];
-        Random r = new Random();
+       // Random r = new Random();
         
-        for (int i = 0; i < mazeFilled.length; i++) {
-            for (int j = 0; j < mazeFilled[0].length; j++) {
-                
-                mazeFilled[i][j]= r.nextInt(2);
-          
-            }
-            
-        }
+//        for (int i = 0; i < mazeFilled.length; i++) {
+//            for (int j = 0; j < mazeFilled[0].length; j++) {
+//                
+//                mazeFilled[i][j]= r.nextInt(2);
+//          
+//            }
+//            
+//        }
+
+        //Llenado de la matriz para el laberinto Facil predeterminado
+//       mazeFilled[0][1] = 1;
+//       mazeFilled[0][2] = 1;
+//       mazeFilled[0][3] = 1;
+//       mazeFilled[0][4] = 1;
+//       mazeFilled[0][5] = 1;
+//       mazeFilled[0][6] = 1;
+//       mazeFilled[0][7] = 1;
+//       mazeFilled[0][8] = 1;
+//       mazeFilled[0][9] = 1;
        
+        for (int i = 0; i < 10; i++) {
+             mazeFilled[0][i] = 1;
+        }
         
+        for (int i = 2; i < 10; i++) {
+             mazeFilled[i][0] = 1;
+        }
+        
+        for (int i = 2; i < 9; i++) {
+             mazeFilled[9][i] = 1;
+        }
+        
+        for (int i = 1; i < 5; i++) {
+             mazeFilled[2][i] = 1;
+        }
+        
+        for (int i = 6; i < 10; i++) {
+             mazeFilled[2][i] = 1;
+        }
+        
+        for (int i = 1; i < 5; i++) {
+             mazeFilled[3][i] = 1;
+        }
+        
+        for (int i = 6; i < 10; i++) {
+             mazeFilled[3][i] = 1;
+        }
+        
+        for (int i = 2; i < 9; i++) {
+             mazeFilled[5][i] = 1;
+        }
+        
+        for (int i = 2; i < 9; i++) {
+             mazeFilled[6][i] = 1;
+        }
+        mazeFilled[7][2] = 1;
+        mazeFilled[7][7] = 1;
+        mazeFilled[7][8] = 1;
+        
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if(mazeFilled[i][i] != 1){
+                    mazeFilled[i][i]=0;
+                }
+            }
+        }
+        
+        // hasta aqui el llenado de la matriz para el laberinto Easy Facil
         
         if(hardMode==true){
         mazeFilled[0][0]=3;
